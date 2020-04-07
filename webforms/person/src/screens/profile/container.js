260698b8019be811688c179c2df88e46b87b1f22
Rowel de Guzman <rowel.deguzman@jukinmedia.com>
2020-04-08 07:23:05 +0800

    fix linter

--- webforms/person/src/screens/profile/container.js
+++ webforms/person/src/screens/profile/container.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-function Container (props) {
+function Container () {
 
   return(
     <div style={{ textAlign: "center" }}>
