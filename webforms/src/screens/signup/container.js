f8f516a8629e9ea04406a19ac84fee51b5978251
Rowel de Guzman <rowel.deguzman@jukinmedia.com>
2020-04-07 23:28:55 +0800

    add signup component

---
+++ webforms/src/screens/signup/container.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Container (props) {
+
+  return(
+    <div style={{ textAlign: "center" }}>
+
+      Registration
+
+    </div>
+  )
+}
+
+Container.propTypes = {
+
+}
+
+export default Container;
--- webforms/src/screens/signup/index.js
+++ webforms/src/screens/signup/index.js
@@ -2,16 +2,11 @@ import { connect } from 'react-redux';
 
 import Container from './container';
 
-import onClickAdd from './handlers/onClickAdd';
-import onClickMinus from './handlers/onClickMinus';
-
 const mapStateToProps = (state) => ({
-  counter: state.screen.Home.counter
 });
 
 const actionCreators = {
-  onClickAdd,
-  onClickMinus
+  
 };
 
 export default connect(mapStateToProps,actionCreators)(Container);
