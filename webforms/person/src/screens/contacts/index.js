76617c03a6bbb1f85b7b1b0969677d65b765d2a2
Patrick Mark Mazo <mpatrickmarkgarcia@gmail.com>
2020-04-08 12:26:10 +0800

    Refactor functions
    1. Change functions to ES6 format
    2. Change data convention
    
     - modified:   webforms/person/src/screens/contacts/container.js
     - modified:   webforms/person/src/screens/contacts/index.js

--- webforms/person/src/screens/contacts/index.js
+++ webforms/person/src/screens/contacts/index.js
@@ -4,23 +4,56 @@ import Container from './container';
 import axios from 'axios'
 
 
-function getContactList(firstname, lastname, contact, address) {
-    return {firstname, lastname, contact, address}
+/**
+ * Get contact list request
+ * [
+ *      {
+ *          "firstName" : "Patrick Mark"
+ *          "lastName" : "Mazo",
+ *          "contactNumber" : "+639361555555",
+ *          "address" : "Quezon City" (Optional)
+ *      }
+ * ]
+ */
+const getContactList = () => {
+    axios.get("")
+      .then(res => {
+    })
 }
 
 const contactList = [
-    getContactList('Patrick Mark', 'Mazo', '+639361555555', 'Quezon City'),
-    getContactList('Joejin', 'Chavez', '+639361555555', 'Quezon City'),
-    getContactList('Rowel', 'de Guzman', '+639361555555', 'Quezon City'),
-    getContactList('Jhon Patrick', 'Jampolina', '+639361555555', 'Quezon City'),
+    {
+        "firstName" : 'Patrick Mark', 
+        "lastName" : 'Mazo', 
+        "contactNumber" : '+639361555555',
+        "address" : 'Quezon City'
+    },
+    {
+        "firstName" : 'Rowel', 
+        "lastName" : 'de Guzman', 
+        "contactNumber" : '+639361555555',
+        "address" : 'Quezon City'
+    },
+    {
+        "firstName" : 'Joejin', 
+        "lastName" : 'Chavez', 
+        "contactNumber" : '+639361555555',
+        "address" : 'Quezon City'
+    },
+    {
+        "firstName" : 'Jhon Patrick', 
+        "lastName" : 'Jampolina', 
+        "contactNumber" : '+639361555555',
+        "address" : 'Quezon City'
+    }
 ];
-
 
 const mapStateToProps = (state) => ({
     contactList : contactList
 });
 
 const actionCreators = {
+    getContactList
 };
 
 
