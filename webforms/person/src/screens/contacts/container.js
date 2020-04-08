a2192d3adf139c566814df099183de7bbf4efb2a
Patrick Mark Mazo <mpatrickmarkgarcia@gmail.com>
2020-04-08 12:04:40 +0800

    Refactor Imports
    1. Modify import initialization
     - modified:   src/screens/contacts/container.js

--- webforms/person/src/screens/contacts/container.js
+++ webforms/person/src/screens/contacts/container.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import { 
+  Table,TableBody,TableCell,
+  TableContainer,TableHead,TableRow,Paper,
+} from '@material-ui/core';
 
 
 const useStyles = makeStyles({
