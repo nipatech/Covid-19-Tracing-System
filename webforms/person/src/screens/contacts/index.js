edeb99fd92932abf4d8037016738ca160a93316d
Jeffrey Ver <jeffreyver@Jeffreys-MacBook-Pro.local>
2020-04-11 17:01:08 +0800

    add atuhorization

--- webforms/person/src/screens/contacts/index.js
+++ webforms/person/src/screens/contacts/index.js
@@ -3,9 +3,11 @@ import ContactContainer from './container';
 
 const info = localStorage.getItem("token");
 const details = JSON.parse(info).signInUserSession.idToken.payload;
+const jwtToken = JSON.parse(info).signInUserSession.idToken.jwtToken;
 
 const mapStateToProps = (state) => ({
-    details : details
+    details : details,
+    jwtToken : jwtToken,
 });
 
 export default connect(mapStateToProps)(ContactContainer);
