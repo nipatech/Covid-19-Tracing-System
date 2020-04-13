8fd27a4ad549a6a848860b118f47c122c400dc1b
Jeffrey Ver <jeffreyver@Jeffreys-MacBook-Pro.local>
2020-04-13 14:50:06 +0800

    profile

--- webforms/person/src/screens/profile/container.js
+++ webforms/person/src/screens/profile/container.js
@@ -1,103 +1,227 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-
-// import PropTypes from 'prop-types';
-
+import React, { useEffect, useState, Fragment } from 'react';
+import axios from "axios";
+import PropTypes from 'prop-types';
+import EditIcon from '@material-ui/icons/Edit'
+import DeleteIcon from '@material-ui/icons/Delete'
 import { 
-  TextField, Avatar, CardActions,
-  Card, CardContent, Button, Grid,
-  Typography, CssBaseline, Container 
+  Table,TableBody,TableCell,TableContainer,
+  TableHead,TableRow,Paper, Button, TextField,
+  Dialog, DialogActions, DialogContent, DialogTitle,
+  Container,IconButton
 } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth:"325px",
-    border : "1px solid #eee",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  large: {
-    width: "100%",
-    height: "100%",
-  },
-  avatar_content: {
-    alignItems:'center',
-    justifyContent:'center',
-    width:"100%",
-    height:"230px"
-  },
-  margin: {
-    margin: theme.spacing(1),
-  },
-}));
+function ContactContainer (props) {
 
-function ProfileContainer (props) {
-  const classes = useStyles();
+  // const {getContactList } = props;
+  const [info, setData] = useState({});
+  const [open, setOpen] = React.useState(false);
+  const [edit, setEdit] = React.useState(false);
+  const [editInfo, seteditInfo] = useState({});
+  const handleChange = name => event => setState({...state, [name]: event.target.value});
+  const [state, setState] = useState({
+    fullName: "",
+    contactNumber: "",
+  });
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleUpdate = (nextProps) => {
+    setEdit(true)
+    seteditInfo(nextProps)
+  };
+
+  const getContactList = async () => {
+    console.log(props);
+    await axios.post(`${process.env.REACT_ENDPOINT}/view-contact-person`, {
+        ppk:  props.details.sub,
+    },{
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': props.jwtToken
+      }
+    }).then(response => {
+      setData({ data:  response.data.Items })
+    }).catch(error => {
+    console.log(error)
+    });
+  }
+
+  const onClickCreateContact = async () => {
+    axios.post(`${process.env.REACT_ENDPOINT}/create-contact-person`, {
+      ppk:  props.details.sub,
+      n:    state.fullName,
+      pn:   state.contactNumber,
+    },{
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': props.jwtToken
+      }
+    }).then(response => {
+      handleClose()
+      getContactList()
+    }).catch(error => {
+      console.log(error)
+    });
+  }
+  const infoHandler = name => event => seteditInfo({...editInfo, [name]: event.target.value});
+  const onClickUpdateContact = async () => {
+    axios.post(`${process.env.REACT_ENDPOINT}/update-contact-person`,{
+      ppk:  props.details.sub,
+      n:    editInfo.full_name,
+      ts :  editInfo.timestamp.toString(),
+      pn:   editInfo.phone_number,
+    },{
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': props.jwtToken
+      }
+      }).then(response => {
+      handleClose()
+      getContactList()
+    }).catch(error => {
+      console.log(error)
+    });
+  }
+
+  const onClickDeleteContact = async (nextprops) => {
+    await axios.post(`${process.env.REACT_ENDPOINT}/delete-contact-person`, {
+        ppk:  props.details.sub,
+        ts : nextprops.timestamp
+    },{
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': props.jwtToken
+      }
+    }).then(response => {
+      getContactList()
+    }).catch(error => {
+    console.log(error)
+    });
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+    setEdit(false);
+
+  };
+  useEffect(() => {
+    getContactList()
+    console.log();
+  },[]);
 
   return(
     <React.Fragment>
-      <CssBaseline />
-      <Container fixed>
-      <Grid container spacing={2}>
-        <Grid item xs={12} >
-          <Card className={classes.root}>
-            <CardContent>
-              <Grid item sm={12} xs={12} className={classes.avatar_content} >
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" className={classes.large} />
-              </Grid>
+      <Container maxWidth="xl">
+        <div style={{display : "flex", marginBottom : "20px"}}>
+          <Button variant="contained" style={{marginLeft : "auto"}} color="primary" onClick={handleClickOpen}>
+            NEW CONTACT
+          </Button>
+        </div>
+        <TableContainer component={Paper}>
+          <Table aria-label="Contact Table">
+            <TableHead>
+              <TableRow>
+                <TableCell  width='130' align="left"></TableCell>
+                <TableCell>Full Name</TableCell>
+              </TableRow>
+            </TableHead>
+            { !info.data  ? (
+               <TableBody>
+                 <TableRow >
+                   <TableCell component="th" scope="row"></TableCell>
+                   <TableCell align="left"></TableCell>
+                 </TableRow>
+             </TableBody>
+            ) : (
+              <TableBody>
+                { info.data.map((row) => (
+                  <TableRow key={row.full_name}>
+                     <TableCell align="left"> 
+                     <IconButton variant="contained" style={{marginLeft : "auto"}} onClick={() => handleUpdate(row)}><EditIcon /></IconButton>
+                     <IconButton variant="contained" style={{marginLeft : "auto"}} onClick={() => onClickDeleteContact(row)}><DeleteIcon /></IconButton>
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {row.full_name} <br></br>
+                      {row.phone_number}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            )}
+          </Table>
+        </TableContainer> 
 
-              <Grid item sm={12} xs={12} >
- 
-              </Grid>
-
-              <TextField
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">New Contact</DialogTitle>
+            <DialogContent>
+            <TextField
                 autoFocus
                 margin="dense"
-                id="fullname"
+                id="fullName"
                 label="Full Name"
+                value={state.fullName}
+                onChange={handleChange("fullName")}
                 type="text"
                 fullWidth
-                value={props.fullname}
                 required
               />
               <TextField
-                autoFocus
                 margin="dense"
-                id="contactNo"
-                label="Contact No"
+                id="contactNumber"
+                label="Contact Number"
+                value={state.contactNumber}
+                onChange={handleChange("contactNumber")}
                 type="text"
                 fullWidth
-                value={props.contactNo}
+                required
+              />
+            </DialogContent>
+        
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">Cancel</Button>
+            <Button onClick={onClickCreateContact} color="primary">Save</Button>
+          </DialogActions>
+        </Dialog>
+        <Dialog open={edit} onClose={handleClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Update Contact</DialogTitle>
+            <DialogContent>
+            <TextField
+                autoFocus
+                margin="dense"
+                id="fullName"
+                label="Full Name"
+                value={editInfo.full_name}
+                onChange={infoHandler("full_name")}
+                type="text"
+                fullWidth
                 required
               />
               <TextField
-                autoFocus
                 margin="dense"
-                id="caseId"
-                label="Case ID"
+                id="contactNumber"
+                label="Contact Number"
+                value={editInfo.phone_number}
+                onChange={infoHandler("phone_number")}
                 type="text"
                 fullWidth
-                value={props.caseId}
                 required
               />
-         
-            </CardContent>
-            <CardActions>
-            </CardActions>
-          </Card>
-        </Grid>
-      </Grid>
+            </DialogContent>
+        
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">Cancel</Button>
+            <Button onClick={onClickUpdateContact} color="primary">Save</Button>
+          </DialogActions>
+        </Dialog>
+
       </Container>
     </React.Fragment>
   )
+} 
+
+ContactContainer.propTypes = {
+
 }
 
-ProfileContainer.propTypes = {
-
-}
-
-export default ProfileContainer;
+export default ContactContainer;
